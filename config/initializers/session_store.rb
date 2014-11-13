32eb48e9c9af75c48e59deb41acc00dba9e2c29f
Stephen Clark <steve@platphormcorp.com>
2014-11-14 09:47:00 +1300

    re #12067 - discourse - Add SSO support to discourse and main app
    Set DC session expiry the same as NF - 2h

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Discourse::Application.config.session_store :cookie_store, key: '_forum_session'
+# PH_CUSTOMIZATIONS: Ensure DC and NF expire session cookies after the same length of time
+# to prevent the DC login from lasting longer than NF (if the user just leaves the browser)
+Discourse::Application.config.session_store :cookie_store, {key: '_forum_session', expire_after: 120.minutes}
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
